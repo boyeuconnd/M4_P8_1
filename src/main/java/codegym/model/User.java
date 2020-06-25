81a59340cb55e3972d8171fffeb935939d766e28
Dang Pham <thienvu453@gmail.com>
2020-06-25 10:08:05 +0700

    Add project and first commit

---
+++ src/main/java/codegym/model/User.java
@@ -0,0 +1,38 @@
+package codegym.model;
+
+import javax.validation.constraints.*;
+
+
+
+public class User {
+    @NotNull
+    @Size(min = 2,max = 30)
+    private String name;
+
+    @Min(18)
+    private int age;
+
+    public User() {
+    }
+
+    public User(@NotNull @Size(min = 2, max = 30) String name, @Min(18) int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
